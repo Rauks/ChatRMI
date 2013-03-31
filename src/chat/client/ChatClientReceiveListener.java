cf69551bdb68d49f1433beafbcfb4cb9f4f040ff
Karl Woditsch <k.woditsch@gmail.com>
2013-03-31 17:26:31 +0200

    Receive events & listeners

---
+++ src/chat/client/ChatClientReceiveListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat.client;
+
+/**
+ *
+ * @author Karl
+ */
+public interface ChatClientReceiveListener {
+    public void receive(ChatClientReceiveEvent evt);
+}
