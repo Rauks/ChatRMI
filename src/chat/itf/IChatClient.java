fc40560a070f95e11ada35175ff432b64c467404
Karl Woditsch <k.woditsch@gmail.com>
2013-03-31 12:38:58 +0200

    Interfaces methods

--- src/chat/itf/IChatClient.java
+++ src/chat/itf/IChatClient.java
@@ -11,5 +11,24 @@ import java.rmi.Remote;
  * @author Karl
  */
 public interface IChatClient extends Remote{
+    /**
+     * Connect the client to a server designed by his host.
+     * 
+     * @param host The chat server's host.
+     */
+    public void connect(String host);
     
+    /**
+     * Send a message to the server.
+     * 
+     * @param msg The message to send.
+     */
+    public void send(String msg);
+    
+    /**
+     * Reveive a message from the server.
+     * 
+     * @param msg The message to receive.
+     */
+    public void receive(String msg);
 }
--- src/chat/itf/IChatServer.java
+++ src/chat/itf/IChatServer.java
@@ -11,5 +11,17 @@ import java.rmi.Remote;
  * @author Karl
  */
 public interface IChatServer extends Remote{
+    /**
+     * Register a client to the server's client list.
+     * 
+     * @param client The client to register.
+     */
+    public void register(IChatClient client);
     
+    /**
+     * Send a message to all the server's clients.
+     * 
+     * @param msg The message to send.
+     */
+    public void disparch(String msg);
 }
